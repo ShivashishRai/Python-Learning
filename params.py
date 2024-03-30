eac410dab5795aebaf84fa861a28e9ca652b1629
ShivashishRai <133220256+ShivashishRai@users.noreply.github.com>
2024-03-30 12:17:57 +0530

    Update params.py

--- params.py
+++ params.py
@@ -1,12 +1,11 @@
 import pytest
 
-
+#defining the fixture with passing parameters
 @pytest.fixture(params=[("Shiv",191),("Puju","MyLove")])
-
 def myfix(request):
-    return request.param
+    return request.param              #request is the instance object for the fixture use whenever we pass param
 
-
-
+#invocing the fixture 
 def testcallfix(myfix):
+    
     print(myfix[0])
